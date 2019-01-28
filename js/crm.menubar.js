7547ff74ea53471c17b05801eb2a5119e83df4e3
Rich Lott <forums@artfulrobot.uk>
2019-01-28 15:48:23 +0000

    remove console.log calls; fix element selection

--- js/crm.menubar.js
+++ js/crm.menubar.js
@@ -50,18 +50,17 @@
         $('#civicrm-menu').trigger('crmLoad');
         $(document).ready(function() {
           handleResize();
-          $('#civicrm-menu a').not('a[href="#"]').not('a[href="#hidemenu"]')
-          .on('click', function(e) {
-            if (e.ctrlKey || e.altKey || e.shiftKey || e.metaKey) {
-              // Prevent menu closing when link is clicked with a keyboard modifier.
-              e.stopPropagation();
-            }
-          });
           $('#civicrm-menu')
             .on('click', 'a[href="#"]', function() {
               // For empty links - keep the menu open and don't jump the page anchor
-              console.warn("stopped #");
               return false;
+            })
+            .on('click', 'a', function(e) {
+              if (! $(this).attr('href').match(/^#/)
+                  && (e.ctrlKey || e.altKey || e.shiftKey || e.metaKey)) {
+                // Prevent menu closing when link is clicked with a keyboard modifier.
+                e.stopPropagation();
+              }
             })
             .on('click', 'a[href="#hidemenu"]', function(e) {
               e.preventDefault();
@@ -69,7 +68,6 @@
             })
             .on('keyup', 'a', function(e) {
               // Simulate a click when spacebar key is pressed
-              console.warn("clicking ");
               if (e.which == SPACE_KEY) {
                 $(e.currentTarget)[0].click();
               }
@@ -117,7 +115,6 @@
         $('#crm-restore-menu')
           .button({icons: {primary: 'fa-undo'}})
           .click(function(e) {
-            console.warn("crm-restore-menu happening");
             e.preventDefault();
             alert.close();
             CRM.menubar.show(speed);
